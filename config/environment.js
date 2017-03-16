910ff8dec71562e7fdf62d3661b5e510277d01b0
Trude <trude.martinsen@gmail.com>
2017-03-16 20:58:34 +0100

    gitignore og test i index.html

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'ember-js-getting-started',
+    modulePrefix: 'easter',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
