e5341c3a41053dca6dd615a5db06023b80b11686
Tommy Kristoffersen <tommykri@gmail.com>
2017-04-11 21:04:59 +0100

    tiril storyline

---
+++ app/controllers/tiril.js
@@ -0,0 +1,47 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  currentQuestion: 0,
+  isTimeForHunt:false,
+
+  a1: null,
+  a2: null,
+  a3: null,
+  a4: null,
+  a5: null,
+
+  isBeforeGametime: function(){
+    var date = new Date();
+    var hour = date.getHours();
+    return hour < 20;
+  }.property('isTimeForHunt'),
+
+  allAnswersCorrect: function(){
+    return this.get('a1') === 5 && this.get('a2') === 2;
+  }.property('a1', 'a2', 'a3', 'a4', 'a5'),
+
+  actions: {
+    beginHunt: function(){
+      this.set('isTimeForHunt', true);
+    },
+    vampireAnswered: function(answer){
+
+    },
+    GotoQ1: function(){
+      this.set('a1', null);
+      this.set('dino', null);
+      this.set('a2', null);
+      this.set('pokemon', null);
+      this.set('a3', null);
+      this.set('a4', null);
+      this.set('a5', null);
+      this.set('currentQuestion', 1);
+    },
+    GotoQ2: function(){
+        this.set('currentQuestion', 2);
+    },
+    GotoQ3: function(){
+        this.set('currentQuestion', 3);
+    },
+  }
+});
